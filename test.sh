3634165acbf8fbb16d2c97db2c149d712f77d5bd
Alexander Zvorygin <grafetu@gmail.com>
2019-01-19 23:00:58 -0800

    test that binaries actually execute

--- test.sh
+++ test.sh
@@ -18,18 +18,34 @@ fi
 
 string="$(alias ls)"
 if [[ $string == *"$exa_binary"* ]]; then
-  echo "okay: exa"
+  echo "okay: exa alias"
 else
-  echo "err: exa"
+  echo "err: exa alias"
+  exit 1
+fi
+string="$(ls)"
+if [ $? -eq 0 ]
+then
+  echo "okay: ls execution"
+else
+  echo "err: ls execution"
   exit 1
 fi
 
 
 string="$(alias cat)"
 if [[ $string == *"$bat_binary"* ]]; then
-  echo "okay: bat"
+  echo "okay: bat alias"
 else
-  echo "err: fzf"
+  echo "err: bat alais"
+  exit 1
+fi
+string="$(cat /dev/null)"
+if [ $? -eq 0 ]
+then
+  echo "okay: cat execution"
+else
+  echo "err: cat execution"
   exit 1
 fi
 
