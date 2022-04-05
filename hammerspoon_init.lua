f855bb0028dccdd796ced899a63ac51e0df7819b
Alex Zvorygin <grafetu@gmail.com>
2022-04-04 22:23:53 -0700

    Update hammerspoon_init.lua

--- hammerspoon_init.lua
+++ hammerspoon_init.lua
@@ -25,7 +25,6 @@ function bindOptionShiftCtrlHotkeyToApp(key, app)
   bindOptionShiftCtrlHotkey(key, function() switchToAndFromApp(app) end, app)
 end
 
-
 function bindShiftCtrlHotkey(key, pressedfn, msg)
   msg = msg or "" 
   hs.hotkey.bind({"shift", "ctrl"}, key, msg, pressedfn, nil, nil)
@@ -35,13 +34,12 @@ function bindShiftCtrlHotkeyToApp(key, app)
   bindShiftCtrlHotkey(key, function() switchToAndFromApp(app) end, app)
 end
 
+
 -- run `osascript -e 'id of app "Slack"'` to get bundle of an app
 
---bindOptionShiftCtrlHotkeyToApp('4', 'com.spotify.client')
---bindOptionShiftCtrlHotkeyToApp('3', 'com.jetbrains.WebStorm')
-
-
-bindHyperHotkeyToApp('4', "com.tinyspeck.slackmacgap")
-bindHyperHotkeyToApp('3', "com.jetbrains.intellij")
-bindHyperHotkeyToApp('2', "com.googlecode.iterm2")
-bindHyperHotkeyToApp('1', "com.google.Chrome")
+bindOptionShiftCtrlHotkeyToApp('4', 'com.spotify.client')
+bindShiftCtrlHotkeyToApp('5', "md.obsidian")
+bindShiftCtrlHotkeyToApp('4', "com.tinyspeck.slackmacgap")
+bindShiftCtrlHotkeyToApp('3', "com.jetbrains.intellij")
+bindShiftCtrlHotkeyToApp('2', "com.googlecode.iterm2")
+bindShiftCtrlHotkeyToApp('1', "com.google.Chrome")
